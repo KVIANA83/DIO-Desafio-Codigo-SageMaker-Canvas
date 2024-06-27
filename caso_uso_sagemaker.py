e7d93528c9d2efa30e03e7b669233bfab3af4335
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-26 23:09:39 -0300

    Atualizar o caso_uso_sagemaker.py
    update

--- caso_uso_sagemaker.py
+++ caso_uso_sagemaker.py
@@ -1,43 +1,19 @@
 # Recebe a Entrada do usuário e armazena na variável "entrada"
-
 entrada = input()
 
-
-
 # Função responsável por receber um caso de uso e retornar sua respectiva descrição.
-
 def descrever_casos(caso):
-
-	if caso == "Resumir o conteúdo":
-
-			return "Produza resumos concisos com as informações mais importantes"
-
-			
-
-	# TODO: Preencha corretamente a descrição de cada caso, considerando as condições abaixo e Saídas possíveis:			
-
-	elif caso == "Gerar conteúdo de vendas e marketing":
-
-	    return "Crie conteúdo de vendas e marketing personalizado"
-
-	    
-
-	elif caso == "Planejar o inventário com eficiência":
-
-	    return "Preveja os níveis do inventário combinando dados"	
-
-	
-
-	elif caso == "Descubra padrões de rotatividade de clientes":
-
-	    return "Descubra padrões de rotatividade de clientes"
-
-	    	    
-
-	elif caso == "Extrair informações de documentos":
-
-	    return "Analise e extraia informações de uma variedade de documentos"
-
+    if caso == "Resumir o conteúdo":
+        return "Produza resumos concisos com as informações mais importantes"
+    elif caso == "Gerar conteúdo de vendas e marketing":
+        return "Crie conteúdo de vendas e marketing personalizado"
+    elif caso == "Planejar o inventário com eficiência":
+        return "Preveja os níveis do inventário combinando dados"
+    elif caso == "Descubra padrões de rotatividade de clientes":
+        return "Descubra padrões de rotatividade de clientes"
+    elif caso == "Analisar documentos":
+        return "Analise e extraia informações de uma variedade de documentos"
+    else:
+        return "Caso de uso desconhecido"
  
-
 print(descrever_casos(entrada))
